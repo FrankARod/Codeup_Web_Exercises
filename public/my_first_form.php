585b94aa0284054e9cfb7e5a972479586a5cfb4f
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-25 11:24:17 -0500

    HTML form test

---
+++ public/my_first_form.php
@@ -0,0 +1,21 @@
+<?php
+
+	var_dump($_POST);
+	var_dump($_GET);
+
+?>
+
+
+<form method="GET" action="/my_first_form.php">
+    <p>
+        <label for="username">Username</label>
+        <input id="username" name="username" type="text">
+    </p>
+    <p>
+        <label for="password">Password</label>
+        <input id="password" name="password" type="password">
+    </p>
+    <p>
+        <input type="submit">
+    </p>
+</form>
