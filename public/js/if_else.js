4f5c59891ea57a7f0cde43b62e235d2d48eb6eee
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-29 11:17:29 -0500

    Initial commit

---
+++ public/js/if_else.js
@@ -0,0 +1,11 @@
+var message, success = true;
+
+/*if (success) {
+	message = "Operation was successful";
+} else {
+	message = "Oops, something went wrong.";
+}*/
+
+message = (success) ? "Operation was successful." : "Oops, something went wrong.";
+
+console.log(message);
